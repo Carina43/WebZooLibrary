972616a993753d6c9ecc7dd25b5a685f40d901d6
Carina43 <cag003@edu.zealand.dk>
2025-11-19 11:06:02 +0100

    Event overview razorpage

---
+++ WebZooWeb/Pages/EventPages/EventOverview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebZooWeb.Pages.EventPages
+{
+    public class EventOverviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
