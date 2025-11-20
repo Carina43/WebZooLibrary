ff0d95996c093d60fbab1617f84f844dad66a253
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-11-20 12:07:44 +0100

    Calendar for next month. link to Calendar Page

--- WebZooWeb/Pages/EventPages/EventCalendar.cshtml.cs
+++ WebZooWeb/Pages/EventPages/EventCalendar.cshtml.cs
@@ -2,20 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebZooLibrary.Service;
 using WebZooLibrary.Model;
+using System.Diagnostics;
 
 namespace WebZooWeb.Pages.EventPages
 {
-    public class EventOverviewModel : PageModel
+    public class EventCalendarModel : PageModel
     {
-        private EventService _eventService = new EventService();
+        private readonly EventService _eventService = new EventService();
 
         [BindProperty]
         public List<Event> Events { get; set; }
         public int Year { get; set; }
         public int Month { get; set; }
         public int Today { get; set; }
+        public int NextMonth { get; set; }
+        public int NextYear { get; set; }
 
-        public EventOverviewModel(EventService eventService)
+        public EventCalendarModel(EventService eventService)
         {
             _eventService = eventService;
             Events = _eventService.GetAll();
@@ -27,6 +30,9 @@ namespace WebZooWeb.Pages.EventPages
             Year = now.Year;
             Month = now.Month;
             Today = now.Day;
+            NextYear = Year;
+            NextMonth = Month++;
+            if(NextMonth == 13) { NextMonth = 1; NextYear++; }
         }
     }
 }
