9945aaf12858e735a9353fca7e799d120b67b01a
thecorf <mim010@edu.zealand.dk>
2025-11-25 10:26:53 +0100

    got the form to work

--- WebZooWeb/Pages/AdminPages/EditAndDeleteEvent.cshtml.cs
+++ WebZooWeb/Pages/AdminPages/EditAndDeleteEvent.cshtml.cs
@@ -10,6 +10,22 @@ namespace WebZooWeb.Pages.AdminPages
     public class EditAndDeleteEventModel : PageModel
     {
         public int editID { set; get; }
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public DateOnly Date { get; set; }
+        [BindProperty]
+        public TimeOnly StartHour { get; set; } = new TimeOnly(14, 0, 0);
+        [BindProperty]
+        public TimeOnly EndHour { get; set; } = new TimeOnly(20, 0, 0);
+        [BindProperty]
+        public int MaxAttendents { get; set; } = 50;
+        [BindProperty]
+        public int CurrentAttendents { get; set; } = 2;
+        [BindProperty]
+        public string Description { get; set; } = "";
+        [BindProperty]
+        public string ImgPath { get; set; } = "";
 
         private readonly EventService _eventService = new EventService();
 
