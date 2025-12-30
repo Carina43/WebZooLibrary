815b81e8276670da6626072ffb42ed6f7320b44f
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-12-30 13:20:28 +0100

    UnitTest EventService.GetAll

---
+++ WebZooLibrary.Tests/Service/EventServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebZooLibrary.Service;
+using WebZooLibrary.Model;
+
+namespace WebZooLibrary.Tests.Service
+{
+    [TestClass]
+    public class EventServiceTests
+    {
+        [TestMethod]
+        public void GetAll_GetEventsFromDB_EventList()
+        {
+            // Arrange
+            EventService eventService = new EventService();
+            List<Event> events = new List<Event>();
+
+            // Act
+            events = eventService.GetAll();
+
+            // Assert
+            Assert.IsTrue(events.Count() > 0);
+        }
+    }
+}
